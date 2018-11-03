dc618a98aed9a343ef70743ef75a8b0eb98c8ae1
Jamil Abbas <jamilabbas787@gmail.com>
2018-11-03 18:33:11 -0400

    6:30

--- Lab11/Lab11/Program.cs
+++ Lab11/Lab11/Program.cs
@@ -7,13 +7,58 @@ using System.Threading.Tasks;
 namespace Lab11
 {
     class Program
-    {
+    { public static string ph = "place holder";     
         static void Main(string[] args)
         {
-            var movies = new Movies("Word", "Other Word");
-            Movies.Title();
+            Console.WriteLine("Welcome to the Movie List Application \n" +
+                "There are 10 movies in this list.");
+            string continueApp = "y";
+            while(continueApp.Equals("y", StringComparison.OrdinalIgnoreCase))
+             {
+                Console.WriteLine("What category are you interested in? (animated, drama, horror, scifi) : ");
+                string selectedCategory = ValidateUserAnswer();
+                Movies movies = new Movies(selectedCategory, null);
+                movies.Category = selectedCategory;
+                movies.SelectMovie(movies);
 
+                Console.WriteLine("Continue? (y/n):  ");
+                continueApp = Console.ReadLine();
+                if (continueApp.Equals("y", StringComparison.OrdinalIgnoreCase))
+                {
+                    continue;
+                }
+            }
+
+            Console.WriteLine("Bye!");
             Console.ReadKey();
+
+        }
+
+        public static string ValidateUserAnswer()
+        {
+            string selectCategory = Console.ReadLine();
+            bool categoryValidation = false;
+
+            while(!categoryValidation)
+            {
+                
+                if (selectCategory.Equals("animated",StringComparison.OrdinalIgnoreCase) 
+                    || selectCategory.Equals("drama", StringComparison.OrdinalIgnoreCase)
+                    || selectCategory.Equals("horror", StringComparison.OrdinalIgnoreCase)
+                    || selectCategory.Equals("scifi", StringComparison.OrdinalIgnoreCase))
+                {
+                    categoryValidation = true;
+                }
+                else
+                {
+                    Console.WriteLine("Category not found. Please enter animate, drama, horror, or scifi:  ");
+                    selectCategory = Console.ReadLine();
+                    categoryValidation = false;
+                }
+
+                
+            }
+                return selectCategory;
         }
     }
 
@@ -22,27 +67,28 @@ namespace Lab11
         private string _title;
         private string _category;
 
-        public Movies(string category, string title)
+        public Movies(string Category, string Title)
         {
-            title = Title;
+            _title = Title;
+            _category = Category;
 
-            
         }
 
         public string Title
         {
-            get { return _title; }
-            set { _title = value; }
+            get;
+            set;
         }
         public string Category
         {
-            get { return _category; }
-            set { _category = value; }
+            get;
+            set;
         }
 
-        public void SelectMovie()
+        public void SelectMovie(Movies movieListSelect)
         {
-            List<Movies> movieList = new List<Movies>();
+            
+            List<Movies> movieList = new List<Movies>(); 
             movieList.Add(new Movies("drama", "Hell or High Water"));
             movieList.Add(new Movies("animated", "Minions"));
             movieList.Add(new Movies("drama", "The Social Network"));
@@ -54,8 +100,24 @@ namespace Lab11
             movieList.Add(new Movies("scifi", "Interstellar"));
             movieList.Add(new Movies("animated", "Frozen"));
 
+            for (int i = 0; i < movieList.Count; i++)
+            {
+                
+                if (movieList[i]._category == Category)
+                {
+                    string Titles;
+                    Titles = movieList[i]._title;
+                    Console.WriteLine( Titles );
+                }
+            }
         }
-            
-
     }
 }
+            
+
+            
+        
+            
+
+    
+
